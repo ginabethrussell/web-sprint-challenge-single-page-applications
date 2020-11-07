35d20c95cff234f1d8a32989b985938c33468ef4
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2020-11-07 12:51:26 -0500

    added routes to home and pizza for form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import{NavLink, Route} from 'react-router-dom';
+import Home from './components/Home';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className='container'>
+      <div className="header">
+        <h1>Lambda Eats</h1>
+        <div className='navlinks'>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/help'>Help</NavLink>
+        </div>   
+      </div>
+      <Route path='/' component={Home} />
+    </div>
   );
 };
 export default App;

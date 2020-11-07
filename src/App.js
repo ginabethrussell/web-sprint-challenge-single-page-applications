d115b3f2035245903a103d1c545087b7f7b99839
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2020-11-07 13:56:24 -0500

    completed form and submitted using axios request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import{NavLink, Route} from 'react-router-dom';
 import Home from './components/Home';
+import Pizza from './components/Pizza';
+import axios from 'axios';
 
 const App = () => {
+  const [order, setOrder]= useState('');
+  
+  const placeOrder = (newOrder)=> {
+    console.log('order', newOrder);
+    setOrder(newOrder);
+  };
+
+  useEffect(() => {
+    console.log(order);
+    axios
+    .post("https://reqres.in/api/users", order)
+    .then(response => {
+      // update temp state with value to display
+      console.log(response);
+
+      // clear state, could also use 'initialState' here
+      setOrder('');
+    })
+    .catch(err => {
+      // this is where we could create a server error in the form!
+      console.log("oops! something happened!");
+    });
+}, [order]);
+
   return (
     <div className='container'>
       <div className="header">
@@ -12,7 +38,8 @@ const App = () => {
           <NavLink to='/help'>Help</NavLink>
         </div>   
       </div>
-      <Route path='/' component={Home} />
+      <Route exact path='/' component={Home} />
+    <Route path='/pizza' render={() => <Pizza placeOrder={placeOrder}/> }/>
     </div>
   );
 };
---
+++ src/components/Pizza.js
@@ -0,0 +1,149 @@
+import React, {useState, useEffect} from 'react';
+import * as yup from 'yup';
+
+const initialPizza = {
+    name: '',
+    size: '',
+    pepperoni: '',
+    sausage: '',
+    onions: '',
+    mushroom: '',
+    instructions: ''
+};
+
+
+export default function Pizza(props){
+    const {placeOrder} = props;
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+    const [errors, setErrors] = useState(initialPizza);
+    const[pizza, setPizza] = useState(initialPizza);
+
+    const formSchema = yup.object().shape({
+        name: yup.string().min(2, "name must be more than 2 letters"),
+        size: yup.string()
+    });
+
+    const validateChange = (e) => {
+        if( e.target.name === 'name' || e.target.name === 'size'){
+            yup
+            .reach(formSchema, e.target.name) // get the value out of schema at key "e.target.name" --> "name="
+            .validate(e.target.value) // value in input
+            .then(valid => {
+            // if passing validation, clear any error
+            setErrors({ ...errors, [e.target.name]: "" });
+            })
+            .catch(err => {
+            // if failing validation, set error in state
+            console.log("error!", err);
+            setErrors({ ...errors, [e.target.name]: err.errors[0] });
+            });
+        };
+    };
+
+    useEffect(() => {
+        formSchema.isValid(pizza).then(valid => {
+          console.log("valid?", valid);
+          setIsButtonDisabled(!valid);
+        });
+      }, [pizza]);
+
+    const submitOrder =(e)=> {
+        e.preventDefault();
+        // console.log(pizza);
+        placeOrder(pizza);
+        setPizza({
+            name: '',
+            size: '',
+            pepperoni: '',
+            sausage: '',
+            onions: '',
+            mushroom: '',
+            instructions: ''}
+        )
+        setButtonDisabled(true);
+    }
+    
+    
+    const onInputChange = (e) => {
+        console.log(e.target.name, e.target.value);
+        e.persist();
+        const newPizza = {
+            ...pizza, 
+            [e.target.name]: e.target.type === "checkbox"? e.target.checked : e.target.value,
+        }
+        if(e.target.type === 'checkbox'){
+            console.log('checkbox', e.target.checked);
+        }
+        validateChange(e);
+        setPizza(newPizza);
+    }
+
+    return(
+        <div className="form">
+            <form onSubmit={submitOrder}>
+                <div className="form-group">
+                    <label htmlFor='name'>Name</label>
+                    <input 
+                        name = 'name'
+                        type='text'
+                        id = 'name'
+                        data-cy='name'
+                        placeholder = 'name'
+                        value = {pizza.name}
+                        required
+                        onChange={onInputChange}
+                     />
+                      {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='size'>Pizza Size</label>
+                    <select id='size' name='size' data-cy='size' value={pizza.size} required onChange={onInputChange}>
+                        <option value=''>--select your size--</option>
+                        <option value='small'>small</option>
+                        <option value='medium'>medium</option>
+                        <option value='large'>large</option>
+                    </select>
+                </div>
+                <div className='form-group'>
+                <label htmlFor="pepperoni" className="toppings">Pepperoni</label>
+                    <input
+                    type="checkbox"
+                    name="pepperoni"
+                    checked={pizza.pepperoni}
+                    onChange={onInputChange}
+                    />
+                    <label htmlFor="sausage" className="toppings">Sausage</label>
+                    <input
+                    type="checkbox"
+                    name="sausage"
+                    checked={pizza.sausage}
+                    onChange={onInputChange}
+                    />
+                    <label htmlFor="onions" className="toppings">Onions</label>
+                    <input
+                    type="checkbox"
+                    name="onions"
+                    checked={pizza.onions}
+                    onChange={onInputChange}
+                    />
+                    <label htmlFor="mushroom" className="toppings">Mushroom</label>
+                    <input
+                    type="checkbox"
+                    name="mushroom"
+                    checked={pizza.mushroom}
+                    onChange={onInputChange}
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='instructions'>Special Instructions</label>
+                    <textarea id='instructions' name='instructions' data-cy='instructions' value={pizza.instructions} onChange={onInputChange}>
+                    </textarea>
+                </div>
+                <button disabled={isButtonDisabled} type="submit">
+                    Add to Order
+                </button>
+            </form>
+        </div>
+    )
+}
